b6957f62272a829cbc4cc8e55495c3e052c1bc44
Samantha Davenport <sdavenport@students.mchenry.edu>
2023-01-29 16:57:54 -0600

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+    var userAns = 0
+
+    do {
+        println("Select your order from the Starbucks Menu below.")
+        println("1) Caramel Frappe")
+        println("2) Black Coffee")
+        println("3) Salted Caramel Cold Brew")
+        println("4) Passion Iced Tea Lemonade")
+        println("5) Chai Latte")
+        println("9) Size Selection")
+        print("Enter your Order Number: ")
+        userAns = readLine()!!.toInt()
+    } while (userAns != 9)
+
+    while (userAns == 9){
+        println("Please select what size $userAns you would like.")
+        println("1) Tall")
+        println("2) Grande")
+        println("3) Vente")
+        print("Enter your Size Number: ")
+        userAns = readLine()!!.toInt()
+    }
+}
