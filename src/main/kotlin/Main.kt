7c57c67df74c44f767202f9f78f314a28263efc5
Samantha Davenport <sdavenport@students.mchenry.edu>
2023-01-29 17:04:51 -0600

    update

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,12 +13,16 @@ fun main(args: Array<String>) {
         userAns = readLine()!!.toInt()
     } while (userAns != 9)
 
-    while (userAns == 9){
-        println("Please select what size $userAns you would like.")
-        println("1) Tall")
-        println("2) Grande")
-        println("3) Vente")
-        print("Enter your Size Number: ")
-        userAns = readLine()!!.toInt()
-    }
+    do {
+        while (userAns != 8){
+            println("Please select what size $userAns you would like.")
+            println("1) Tall")
+            println("2) Grande")
+            println("3) Vente")
+            println("8) Checkout")
+            print("Enter your Size Number: ")
+            userAns = readLine()!!.toInt()
+        }
+    } while (userAns == 9)
+
 }
