ef46a8db139ab5b35d4b779d88aa5ee7ae2b8973
Samantha Davenport <sdavenport@students.mchenry.edu>
2023-02-02 11:37:43 -0600

    update

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,6 @@
 fun main(args: Array<String>) {
     var userAns = 0
-
-    do {
+    while (userAns != 9) {
         println("Select your order from the Starbucks Menu below.")
         println("1) Caramel Frappe")
         println("2) Black Coffee")
@@ -11,18 +10,14 @@ fun main(args: Array<String>) {
         println("9) Size Selection")
         print("Enter your Order Number: ")
         userAns = readLine()!!.toInt()
-    } while (userAns != 9)
-
-    do {
-        while (userAns != 8){
-            println("Please select what size $userAns you would like.")
-            println("1) Tall")
-            println("2) Grande")
-            println("3) Vente")
-            println("8) Checkout")
-            print("Enter your Size Number: ")
-            userAns = readLine()!!.toInt()
-        }
-    } while (userAns == 9)
-
+    }
+    while (userAns != 8) {
+        println("Please select what size $userAns you would like.")
+        println("1) Tall")
+        println("2) Grande")
+        println("3) Vente")
+        println("8) Checkout")
+        print("Enter your Size Number: ")
+        userAns = readLine()!!.toInt()
+    }
 }
